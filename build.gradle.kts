7ade0ced2f31a6b3a6c804e75e7a77ad2f96ad6c
Stanislav Markin <stasmarkin@gmail.com>
2025-12-29 18:43:54 +0300

    build format

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,34 +2,34 @@ import org.jetbrains.intellij.tasks.PublishPluginTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.9.22"
-  // https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html#ide-configuration
-  id("org.jetbrains.intellij") version "1.17.1"
+    kotlin("jvm") version "1.9.22"
+    // https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html#ide-configuration
+    id("org.jetbrains.intellij") version "1.17.1"
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 group = "com.stasmarkin"
 version = "1.1.1"
 
 intellij {
-  type.set("IC")
-  version.set("2024.1")
-  updateSinceUntilBuild.set(false)
+    type.set("IC")
+    version.set("2024.1")
+    updateSinceUntilBuild.set(false)
 }
 
 tasks.withType<PublishPluginTask> {
-  token.set(System.getProperty("PUBLISH_TOKEN"))
+    token.set(project.findProperty("PUBLISH_TOKEN") as String? ?: System.getProperty("PUBLISH_TOKEN"))
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
-  targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
-  kotlinOptions.freeCompilerArgs = kotlinOptions.freeCompilerArgs + listOf("-Xjvm-default=all")
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlinOptions.freeCompilerArgs = kotlinOptions.freeCompilerArgs + listOf("-Xjvm-default=all")
 }
